ff88dd4145b879c3dd18a9c1c502f9484f627346
Simon Chan <cnsimonchan@live.com>
2019-06-19 13:49:27 +0800

    feat: export packet types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as KoshareClient } from './koshare-client';
 export { default as KoshareReconnectClient } from './koshare-reconnect-client';
 export { default as KoshareServer } from './koshare-server';
+export * from './packet';
