4e50f84cd19339946a3e70fc3dec5dc730877914
Simon Chan <cnsimonchan@live.com>
2020-08-28 14:57:13 +0800

    ci: add GitHub workflows

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as KoshareClient } from '@yume-chan/koshare-router-client';
 export * from '@yume-chan/koshare-router-client';
+export { default as KoshareClient } from '@yume-chan/koshare-router-client';
 export { default as default } from './koshare-server';
